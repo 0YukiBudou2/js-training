fac5b6410f7671332161b5b9f3ea311287c21be0
0YukiBudou2 <kirato1783@yahoo.co.jp>
2026-05-05 12:19:34 +0900

    Lesson7

--- src/pages/Lesson07/index.jsx
+++ src/pages/Lesson07/index.jsx
@@ -4,7 +4,45 @@ import Chart from "../../components/Chart07";
 import instruction from "./instruction.md?raw";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  for(const item of input){
+    const d = new Date(`${item.createdAt} UTC`);
+    const j = d.getTime() + 9 * 60 * 60 * 1000;
+    item.createdAt = new Date(j).toISOString().slice(0,10);
+  }
+  const count = {};
+  for(const item of input){
+    if(!count[item.createdAt]){
+      count[item.createdAt] = {tweet: 0,retweet: 0};
+    }
+    if(item.isRetweet){
+      count[item.createdAt]["retweet"]++;
+    }else{
+      count[item.createdAt]["tweet"]++;
+    }
+  }
+ /* const tw = Object.entries(count).map(([date,value]) =>{
+    return{
+      x: date,
+      y: value.tweet
+    }
+  })
+  const rtw = Object.entries(count).map(([date,value]) =>{
+    return{
+      x: date,
+      y: value.rtweet
+    }
+  })  
+  return [{id: "tweet",data: tw,},{id: "retweet",data:rtw}];*/
+  return ["tweet","retweet"].map(item =>{
+    return{
+      id: item,
+      data: Object.entries(count).map(([date,value]) =>{
+        return{
+          x: date,
+          y: value[item]
+        }})
+      }        
+  });
 };
 
 const Lesson = () => {
