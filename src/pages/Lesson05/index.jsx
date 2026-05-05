ec56720f2b53dd5568c773fdc74814b799c21e94
0YukiBudou2 <kirato1783@yahoo.co.jp>
2026-05-05 09:03:59 +0900

    Lesson5

--- src/pages/Lesson05/index.jsx
+++ src/pages/Lesson05/index.jsx
@@ -4,7 +4,23 @@ import Chart from "../../components/Chart05";
 import instruction from "./instruction.md?raw";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  const height = input.map(item => item.y);
+  const min = Math.round(Math.min(...height));
+  const max = Math.round(Math.max(...height));
+  const bin = [];
+  for (let i = 0; i < max-min+1; i++){
+    bin[i] = {
+      bin: (i+min).toString(),
+      男性: 0,
+      女性: 0
+    };
+  }
+  input.forEach(item =>{
+    const h = Math.round(item.y);
+    const index = h-min;
+    bin[index][item.gender]++;
+  });
+  return bin; 
 };
 
 const Lesson = () => {
